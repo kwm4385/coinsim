4428a30969cf36982488a526072b3b144aec39ce
Kevin Moses <kwm4385@rit.edu>
2014-06-06 02:13:11 -0400

    Added price data collection

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,15 +1,45 @@
 package controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import models.User;
+import play.libs.F.Promise;
+import play.libs.F.Function;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import util.PriceData;
 import views.html.dashboard.index;
 
 @Security.Authenticated(Secured.class)
 public class Dashboard extends Controller {
 
+	/**
+	 * Returns the dashboard index.
+	 * @return
+	 */
     public static Result index() {
         return ok(index.render(User.findByEmail(request().username())));
     }
+    
+    /**
+     * Returns a JSON response with the current bitcoin price.
+     * @return
+     */
+    public static Promise<Result> price() {
+    	return PriceData.getPrice().map(new Function<Double, Result>() {
+			@Override
+			public Result apply(Double price) throws Throwable {
+				Map<String, String> response = new HashMap<String, String>();
+				response.put("price", Double.toString(price));
+				response.put("last_updated", PriceData.lastUpdated().toString());
+				response.put("exchange", PriceData.getExchange());
+				return ok(Json.toJson(response));
+			}
+    	});
+    }
+   
+    
 }
---
+++ app/util/PriceData.java
@@ -0,0 +1,91 @@
+package util;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import play.Logger;
+import play.libs.F.Function0;
+import play.libs.F.Promise;
+
+
+/**
+ * Class holding up to date bitcoin price data. \
+ * Always a minute old at the latest.
+ * @author kevin
+ */
+public class PriceData {
+	
+	// URL of the prices api
+	public static final String API_URL = "https://api.bitcoinaverage.com/exchanges/USD";
+	
+	// How long prices are cached in seconds
+	public static final int CACHE_LIFE = 30;
+	
+	private static final PriceUpdater PRICE_UPDATER = new PriceUpdater();
+	
+	private static double price;
+	private static Date lastUpdated = new Date(0L);
+	private static String exchange = "bitstamp";
+	
+	/**
+	 * Gets the last fetched price.
+	 * @return
+	 */
+	public static Promise<Double> getPrice() {
+		return Promise.promise(PRICE_UPDATER);
+	}
+	
+	/**
+	 * Gets the time the price was last updated.
+	 * @return
+	 */
+	public static Date lastUpdated() {
+		return lastUpdated;
+	}
+	
+	/**
+	 * Gets the name of the exchange being used for price quotes.
+	 * @return
+	 */
+	public static String getExchange() {
+		return exchange;
+	}
+	
+	static {
+		try {
+			PRICE_UPDATER.apply();
+		} catch (Throwable e) {
+			Logger.error("Error fetching price data");
+		}
+		Logger.debug("Init price info");
+	}
+	
+	/**
+	 * Updates the price and last updated time. **Blocking**
+	 */
+	private static class PriceUpdater implements Function0<Double> {
+		@Override
+		public Double apply() throws Throwable {
+			if(((new Date().getTime() - lastUpdated.getTime()) / 1000) > CACHE_LIFE) {
+				Logger.debug("Fetching new price");
+				try {
+					URLConnection api = new URL(API_URL).openConnection();
+					ObjectMapper mapper = new ObjectMapper();
+					Map<String, Object> jsonMap = mapper.readValue(api.getInputStream(), Map.class);
+					price = Double.parseDouble(((Map)((Map)jsonMap.get(exchange)).get("rates")).get("ask").toString());
+					Logger.debug(Double.toString(price));
+					lastUpdated = new Date();
+				} catch (Exception e) {
+					Logger.error("Error fetching price data");
+				}
+			} else {
+				Logger.debug("Using cached price");
+			}
+			return price;
+		}
+	}
+}
