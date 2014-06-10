cf3251bc66e811c98ff715e1c4d7d3c67ccc162d
Kevin Moses <kwm4385@rit.edu>
2014-06-10 01:56:12 -0400

    Added last updated data to price alt text

--- public/javascripts/dashboard-base.js
+++ public/javascripts/dashboard-base.js
@@ -5,6 +5,7 @@ $("#menu-toggle").click(function(e) {
 
 $.getJSON("/dashboard/price.json", function(data) {
 	$("#nav-ticker").text("$" + data.price + " USD/BTC");
+	$("#nav-ticker").attr("title", "Last updated: " + data.last_updated);
 	$("#nav-loader").hide();
 	$("#nav-ticker").css('visibility', 'visible');
 });
