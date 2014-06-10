1c48e5b2749411288d97c442a231d0c83c2c86c0
Kevin Moses <kwm4385@rit.edu>
2014-06-09 20:55:13 -0400

    Added sim selection

--- public/javascripts/simulations.js
+++ public/javascripts/simulations.js
@@ -1,5 +1,10 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   $(".money").each(function() {
 	  $(this).text(accounting.formatMoney($(this).text()));
   });
 });
+
+function selectRow(id) {
+	$("tr#"+id).addClass("success");
+	$("tr").not("#"+id).removeClass("success");
+}
